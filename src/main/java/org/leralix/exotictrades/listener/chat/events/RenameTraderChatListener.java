4f5f33a35da475461f894d6aee23ac43198f0f1d
Leralix <78090544+Leralix@users.noreply.github.com>
2025-04-14 01:44:00 +0200

    Fix chatListner not releasing player after renaming a villager

--- src/main/java/org/leralix/exotictrades/listener/chat/events/RenameTraderChatListener.java
+++ src/main/java/org/leralix/exotictrades/listener/chat/events/RenameTraderChatListener.java
@@ -3,6 +3,7 @@ package org.leralix.exotictrades.listener.chat.events;
 import org.bukkit.entity.Player;
 import org.leralix.exotictrades.guis.ManageTrader;
 import org.leralix.exotictrades.listener.chat.ChatListenerEvent;
+import org.leralix.exotictrades.listener.chat.PlayerChatListenerStorage;
 import org.leralix.exotictrades.traders.Trader;
 
 public class RenameTraderChatListener extends ChatListenerEvent {
@@ -14,6 +15,7 @@ public class RenameTraderChatListener extends ChatListenerEvent {
 
     @Override
     public void execute(Player player, String message) {
+        PlayerChatListenerStorage.removePlayer(player);
         trader.setName(message);
         openGui(player1 -> new ManageTrader(player1, trader).open(), player);
     }
